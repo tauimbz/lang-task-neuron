95f273c03badcbcf00753a4ef2b146df77cbc9ea
inayarhmns <inaya.rahmanisa@gmail.com>
2025-05-02 02:44:52 +0700

    commands

--- commands/get_act/gemma_mini.sh
+++ commands/get_act/gemma_mini.sh
@@ -1,16 +1,11 @@
-python get_activations.py  \
+nohup python get_activations.py \
     --hf_logintoken "***REMOVED***" \
-    --model "google/gemma-2-2b-it"\
+    --model "google/gemma-2-2b-it" \
     --dataset_name "Muennighoff/flores200" \
     --split "dev" \
     --max_instances 1000 \
     --apply_template \
-    --max_tokens_overzeros 100000\
+    --max_tokens_overzeros 100000 \
     --kaggle_dataname_to_save "act-gemma2-neurons" \
-    --selected_langs "deu_Latn" "eng_Latn" "fra_Latn" "ind_Latn" "jpn_Jpan" "kor_Hang" "zsm_Latn" "nld_Latn" "por_Latn" "rus_Cyrl" "vie_Latn" "zho_Hans"\
-    # --is_update \
-    # --debug \
-    # --take_whole \
-    # --max_lang \
-    # --selected_langs "deu_Latn" "eng_Latn" "fra_Latn" "ind_Latn" "jpn_Jpan" "kor_Hang" "zsm_Latn" "nld_Latn" "por_Latn" "rus_Cyrl" "vie_Latn" "zho_Hans"\
-    # --is_predict \
+    --selected_langs "deu_Latn" "eng_Latn" "fra_Latn" "ind_Latn" "jpn_Jpan" "kor_Hang" "zsm_Latn" "nld_Latn" "por_Latn" "rus_Cyrl" "vie_Latn" "zho_Hans" \
+    > output.log 2>&1 &
