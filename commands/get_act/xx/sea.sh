7876e092e6cc79cedb161aff38077d9f5d4f6273
inayarhmns <inaya.rahmanisa@gmail.com>
2025-05-13 16:34:13 +0700

    commmms

--- commands/get_act/xx/sea.sh
+++ commands/get_act/xx/sea.sh
@@ -8,5 +8,5 @@ python3 get_activations.py  \
     --selected_langs "eng_Latn" "nld_Latn" "ind_Latn" "zsm_Latn" "vie_Latn" "jpn_Jpan" "zho_Hans" "fra_Latn" "por_Latn" "rus_Cyrl" "est_Latn" "hat_Latn" "ita_Latn" "quy_Latn" "swh_Latn" "tam_Taml" "tha_Thai" "tur_Latn" \
     --batch_size 32 \
     --kaggle_dataname_to_save "activationxx-sea7-flores" \
-    --parent_dir_to_save "workspace/" \
+    --parent_dir_to_save "/workspace/" \
     > sea7_actxx.txt 2>&1
