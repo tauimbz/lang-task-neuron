eb2b73f860058baa0aaf88afa57379477f73f679
inayarhmns <inaya.rahmanisa@gmail.com>
2025-05-14 08:26:49 +0700

    commmms

--- commands/overlap/xx/gemma.sh
+++ commands/overlap/xx/gemma.sh
@@ -2,7 +2,7 @@ python overlap_neurons.py  \
     --data_kaggle_result "inayarahmanisa/lsnxx-gemma9-flores" \
     --lape_filename "gemma9_flores" \
     --res_filename "result_gemma9_flores.pt" \
-    --n_layer 24 \
+    --n_layer 42 \
     --model_name_inf "gemma9" \
     --dataset_name_inf "flores" \
     --kaggle_dataname_to_save "lsnxx-gemma9-flores" \
--- commands/overlap/xx/gemma_mini.sh
+++ commands/overlap/xx/gemma_mini.sh
@@ -2,7 +2,7 @@ python overlap_neurons.py  \
     --data_kaggle_result "inayarahmanisa/lsnxx-gemma2-flores" \
     --lape_filename "gemma2_flores" \
     --res_filename "result_gemma2_flores.pt" \
-    --n_layer 24 \
+    --n_layer 26 \
     --model_name_inf "gemma2" \
     --dataset_name_inf "flores" \
     --kaggle_dataname_to_save "lsnxx-gemma2-flores" \
