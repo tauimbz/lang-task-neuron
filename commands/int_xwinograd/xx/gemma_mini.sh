65ec0ceeec6179b1fd67db6a000028c88b996794
inayarhmns <inaya.rahmanisa@gmail.com>
2025-05-15 06:30:08 +0700

    commmms

--- commands/int_xwinograd/xx/gemma_mini.sh
+++ commands/int_xwinograd/xx/gemma_mini.sh
@@ -16,5 +16,5 @@ python intervention_tasks.py \
     --parent_dir_to_save "" \
     --target_langs 0 7 5 8 9 6 \
     --is_update \
-    --batch_size 8 \
+    --batch_size 6 \
     > gemma_mini_xwinogradxx.txt 2>&1
