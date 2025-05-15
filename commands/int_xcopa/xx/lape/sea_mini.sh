6902874b91024d5c12d83632fb71a1d3df92eaca
inayarhmns <inaya.rahmanisa@gmail.com>
2025-05-15 18:29:13 +0700

    commmms

---
+++ commands/int_xcopa/xx/lape/sea_mini.sh
@@ -0,0 +1,21 @@
+python intervention_tasks.py \
+    --dataset_kaggle "inayarahmanisa/lsnxx-sea1-5-flores" \
+    --lsn_filename "sea1-5_flores" \
+    --ld_filename "lang_dict" \
+    --dataset_kaggle_replacer "inayarahmanisa/activationxx-sea1-5-neurons" \
+    --replacer_filename "max.pt" \
+    --hf_token "***REMOVED***" \
+    --model_name "SeaLLMs/SeaLLMs-v3-1.5B-Chat" \
+    --dataset_name "cambridgeltl/xcopa" \
+    --split test \
+    --replace_method fixed \
+    --operation_non_target ".1" \
+    --operation_target "=10" \
+    --metrics "acc" \
+    --kaggle_dataname_to_save "accxx-xcopa-lape" \
+    --parent_dir_to_save "" \
+    --selected_langs "et" "ht" "id" "it" "qu" "sw" "ta" "th" "tr" "vi" "zh" \
+    --target_langs 10 11 2 12 13 14 15 16 17 4 6 \
+    --is_update \
+    --batch_size 6 \
+    > sea_mini_xcopaxx.txt 2>&1
