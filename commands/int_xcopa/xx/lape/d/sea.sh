8f9f55f1d4527bf5f87483d188402847c4d07bef
inayarhmns <inaya.rahmanisa@gmail.com>
2025-05-15 23:04:45 +0700

    commmms

---
+++ commands/int_xcopa/xx/lape/d/sea.sh
@@ -0,0 +1,19 @@
+python intervention_tasks.py \
+    --dataset_kaggle "inayarahmanisa/lsnxx-sea7-flores" \
+    --lsn_filename "sea7_flores" \
+    --ld_filename "lang_dict" \
+    --hf_token "***REMOVED***" \
+    --model_name "SeaLLMs/SeaLLMs-v3-7B-Chat" \
+    --dataset_name "cambridgeltl/xcopa" \
+    --split test \
+    --replace_method percent \
+    --operation_non_target ".1" \
+    --operation_target "=10" \
+    --metrics "acc" \
+    --kaggle_dataname_to_save "accxx-xcopa-lape-d" \
+    --parent_dir_to_save "" \
+    --selected_langs "et" "ht" "id" "it" "qu" "sw" "ta" "th" "tr" "vi" "zh" \
+    --target_langs 10 11 2 12 13 14 15 16 17 4 6 \
+    --is_update \
+    --batch_size 20 \
+    > sea_xcopaxx.txt 2>&1
---
+++ commands/int_xwinograd/xx/lape/d/sea.sh
@@ -0,0 +1,18 @@
+python intervention_tasks.py \
+    --dataset_kaggle "inayarahmanisa/lsnxx-sea7-flores" \
+    --lsn_filename "sea7_flores" \
+    --ld_filename "lang_dict" \
+    --hf_token "***REMOVED***" \
+    --model_name "SeaLLMs/SeaLLMs-v3-7B-Chat" \
+    --dataset_name "Muennighoff/xwinograd" \
+    --split test \
+    --replace_method percent \
+    --operation_non_target ".1" \
+    --operation_target "=10" \
+    --metrics "acc" \
+    --kaggle_dataname_to_save "accxx-xwinograd-lape-d" \
+    --parent_dir_to_save "" \
+    --target_langs 0 7 5 8 9 6 \
+    --is_update \
+    --batch_size 32 \
+    > sea_xwinogradxx.txt 2>&1
