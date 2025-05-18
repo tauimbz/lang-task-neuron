e83a612e42be0bfb12df49185256ad2c1654381c
inayarhmns <inaya.rahmanisa@gmail.com>
2025-05-18 09:43:09 +0700

    commmms

---
+++ commands/intervention/set10p/gemma_mini.sh
@@ -0,0 +1,21 @@
+python intervention_dod.py \
+    --dataset_kaggle "inayarahmanisa/lsnxx-gemma2-flores" \
+    --lsn_filename "gemma2_flores" \
+    --ld_filename "lang_dict" \
+    --hf_token "***REMOVED***" \
+    --model_name "google/gemma-2-2b-it" \
+    --dataset_name "inayarhmns/MLAMA-dod-185" \
+    --split "test" \
+    --replace_method "percent" \
+    --operation_non_target ".1" \
+    --operation_target "=10" \
+    --metrics "dod" \
+    --kaggle_dataname_to_save "dod-lape" \
+    --is_update \
+    --parent_dir_to_save "" 
+# --show_df_per_lang false \
+# --range_layers None \
+# --target_langs None \
+# --max_samples 2 \
+# --apply_template false \
+
---
+++ commands/intervention/setmax/sea_mini.sh
@@ -0,0 +1,21 @@
+python intervention_dod.py \
+    --dataset_kaggle "inayarahmanisa/lsnxx-sea1-5-flores" \
+    --lsn_filename "sea1-5_flores" \
+    --ld_filename "lang_dict" \
+    --hf_token "***REMOVED***" \
+    --model_name "SeaLLMs/SeaLLMs-v3-1.5B-Chat" \
+    --dataset_name "inayarhmns/MLAMA-dod-185" \
+    --split "test" \
+    --replace_method "max" \
+    --operation_non_target ".1" \
+    --operation_target "=1" \
+    --metrics "dod" \
+    --kaggle_dataname_to_save "dod-lape" \
+    --is_update \
+    --parent_dir_to_save ""
+# --show_df_per_lang false \
+# --range_layers None \
+# --target_langs None \
+# --max_samples 2 \
+# --apply_template false \
+
