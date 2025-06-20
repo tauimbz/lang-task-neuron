cf2c6e493297ebf61735e6d868cf39f39ad4280d
inayarhmns <inaya.rahmanisa@gmail.com>
2025-06-20 11:34:11 +0700

    raw neurons

---
+++ commands/raw_act_neurons/gemma_mini.sh
@@ -0,0 +1,7 @@
+python raw_act_neurons.py  \
+    --data_kaggle_result "inayarahmanisa/activationxx-gemma2-neurons" \
+    --act_filename "act_flores200_997_False.pt" \
+    --ld_filename "ld_flores200" \
+    --save \
+    --parent_dir_to_save "" \
+    --kaggle_dataname_to_save "activationxx-gemma2-neurons" \
---
+++ commands/raw_act_neurons/sea.sh
@@ -0,0 +1,7 @@
+python raw_act_neurons.py  \
+    --data_kaggle_result "inayarahmanisa/activationxx-sea7-flores" \
+    --act_filename "act_flores200_997_False.pt" \
+    --ld_filename "ld_flores200" \
+    --save \
+    --parent_dir_to_save "" \
+    --kaggle_dataname_to_save "activationxx-sea7-flores" \
