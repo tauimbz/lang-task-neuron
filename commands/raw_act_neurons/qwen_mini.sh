e4e4eb240116067875e435a7c42aa3843d8493c4
inayarhmns <inaya.rahmanisa@gmail.com>
2025-06-20 11:17:10 +0700

    raw neurons

--- commands/raw_act_neurons/qwen_mini.sh
+++ commands/raw_act_neurons/qwen_mini.sh
@@ -1,4 +1,4 @@
-python overlap_neurons.py  \
+python raw_act_neurons.py  \
     --data_kaggle_result "inayarahmanisa/activationxx-qwen05-neurons" \
     --act_filename "act_flores200_997_False.pt" \
     --ld_filename "ld_flores200" \
