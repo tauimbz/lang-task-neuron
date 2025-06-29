44e63632919b867368764a4145bbe47e00a0b149
inayarhmns <inaya.rahmanisa@gmail.com>
2025-06-29 19:03:58 +0700

    hhhhhhhhhhhhhhhhh

---
+++ commands/int_xwinograd/xx/ap/gemma_mini.sh
@@ -0,0 +1,20 @@
+python intervention_tasks.py \
+    --dataset_kaggle "inayarahmanisa/lsnxx-gemma2-flores" \
+    --lsn_filename "map_t354_gemma2_flores.pt" \
+    --ld_filename "lang_dict" \
+    --dataset_kaggle_replacer "inayarahmanisa/activationxx-gemma2-neurons" \
+    --replacer_filename "max.pt" \
+    --hf_token "***REMOVED***" \
+    --model_name "google/gemma-2-2b-it" \
+    --dataset_name "Muennighoff/xwinograd" \
+    --split test \
+    --replace_method fixed \
+    --operation_non_target ".1" \
+    --operation_target "=22" \
+    --metrics "acc" \
+    --kaggle_dataname_to_save "accxx-xwinograd-ap" \
+    --parent_dir_to_save "" \
+    --target_langs 0 7 5 8 9 6 \
+    --is_update \
+    --batch_size 4 \
+    > gemma_mini_xwinogradxx.txt 2>&1
---
+++ commands/int_xwinograd/xx/ap/sea.sh
@@ -0,0 +1,20 @@
+python intervention_tasks.py \
+    --dataset_kaggle "inayarahmanisa/lsnxx-sea7-flores" \
+    --lsn_filename "map_tb1426_sea7_flores.pt" \
+    --ld_filename "lang_dict" \
+    --dataset_kaggle_replacer "inayarahmanisa/activationxx-sea7-flores" \
+    --replacer_filename "max.pt" \
+    --hf_token "***REMOVED***" \
+    --model_name "SeaLLMs/SeaLLMs-v3-7B-Chat" \
+    --dataset_name "Muennighoff/xwinograd" \
+    --split test \
+    --replace_method fixed \
+    --operation_non_target ".1" \
+    --operation_target "=10" \
+    --metrics "acc" \
+    --kaggle_dataname_to_save "accxx-xwinograd-ap" \
+    --parent_dir_to_save "" \
+    --target_langs 0 7 5 8 9 6 \
+    --is_update \
+    --batch_size 32 \
+    > sea_xwinogradxx.txt 2>&1
