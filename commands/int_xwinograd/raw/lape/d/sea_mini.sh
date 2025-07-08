cdb33fc3ce9c260ec1c306bf33b919e1dd9ec5e6
inayarhmns <inaya.rahmanisa@gmail.com>
2025-07-08 13:39:37 +0700

    small raw

---
+++ commands/int_xwinograd/raw/lape/d/sea_mini.sh
@@ -0,0 +1,18 @@
+python intervention_tasks.py \
+    --dataset_kaggle "inayarahmanisa/lsnxx-sea1-5-flores" \
+    --lsn_filename "sea1-5_flores" \
+    --ld_filename "lang_dict" \
+    --hf_token "***REMOVED***" \
+    --model_name "SeaLLMs/SeaLLMs-v3-1.5B-Chat" \
+    --dataset_name "Muennighoff/xwinograd" \
+    --split test \
+    --replace_method percent \
+    --operation_non_target ".1" \
+    --operation_target "=10" \
+    --metrics "acc" \
+    --kaggle_dataname_to_save "accxx-xwinograd-lape-d" \
+    --parent_dir_to_save "" \
+    --target_langs 0 7 5 8 9 6 \
+    --is_update \
+    --batch_size 4 \
+    > sea_mini_xwinogradxx.txt 2>&1
---
+++ commands/int_xwinograd/raw/lape/qwen_mini.sh
@@ -0,0 +1,20 @@
+python intervention_tasks.py \
+    --dataset_kaggle "inayarahmanisa/activationxx-qwen05-neurons" \
+    --lsn_filename "raw_act_lsn_qwen05.pt" \
+    --ld_filename "ld_flores200" \
+    --dataset_kaggle_replacer "inayarahmanisa/activationxx-qwen05-neurons" \
+    --replacer_filename "max.pt" \
+    --hf_token "***REMOVED***" \
+    --model_name "Qwen/Qwen2.5-0.5B-Instruct" \
+    --dataset_name "Muennighoff/xwinograd" \
+    --split test \
+    --replace_method fixed \
+    --operation_non_target ".1" \
+    --operation_target "=22" \
+    --metrics "acc" \
+    --kaggle_dataname_to_save "accxx-xwinograd-raw" \
+    --parent_dir_to_save "" \
+    --target_langs 0 7 5 8 9 6  \
+    --is_update \
+    --batch_size 8 \
+    > qwen_mini_xwinogradxx.txt 2>&1
