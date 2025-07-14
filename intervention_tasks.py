a8fea1b9dc61a3915f53ba2cd496f51339bfbf0d
inayarhmns <inaya.rahmanisa@gmail.com>
2025-07-14 17:04:25 +0700

    include

--- intervention_tasks.py
+++ intervention_tasks.py
@@ -803,7 +803,9 @@ def HF_calculate_answer(ds, data, dataset_name, model, eval_type, is_generate, d
         ref = choices[1]
         lang_code = lang_target.split("_", 1)[-1]
         lang_text = map_language(lang_code)
-        source = f"Translate the following from English to {lang_text}. English: {choices[0]}\n{lang_text}: "
+        # source = f"Translate the following from English to {lang_text}. English: {choices[0]}.\n{lang_text}: "
+        source = f"Translate from English to {lang_text}.\n\nEnglish: {choices[0]}\n{lang_text}:"
+
         # [choices[0]] is sentence in eng_Latn
         #  choices[1] is sentence from the target language
         return eval_type, source, [ref], is_generate
