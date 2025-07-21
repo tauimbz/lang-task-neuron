31dc2680a18576222896875748ba35123ff51f9a
inayarhmns <inaya.rahmanisa@gmail.com>
2025-07-21 14:42:53 +0700

    f

--- commands/lape/xx/xwin/qwen.sh
+++ commands/lape/xx/xwin/qwen.sh
@@ -1,6 +1,6 @@
 python lape.py  \
     --in_kaggle \
-    --dataset_kaggle "inayarahmanisa/activationxx-qwen7-flores"\
+    --dataset_kaggle "inayarahmanisa/activation-xwin-qwen7-neurons"\
     --filename "oz_xwinograd_83_False" \
     --model_name_inf "qwen7" \
     --dataset_name_inf "flores" \
