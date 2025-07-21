95d42ca839a3da7fb74742a198d20128a669965c
inayarhmns <inaya.rahmanisa@gmail.com>
2025-07-21 13:14:04 +0700

    f

--- commands/get_act/xwin/qwen.sh
+++ commands/get_act/xwin/qwen.sh
@@ -7,7 +7,7 @@ python3 get_activations.py  \
     --max_sentence_avgs 83\
     --batch_size 16 \
     --kaggle_dataname_to_save "activation-xwin-qwen7-neurons" \
-    --parent_dir_to_save "workspace/" \
+    --parent_dir_to_save "" \
     > workspace/log_qwen7.txt 2>&1
     # --max_instances 200 \
     # --is_update \
