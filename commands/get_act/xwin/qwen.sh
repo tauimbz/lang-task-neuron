485de28b96d9154b05f125660b322516396cbf49
inayarhmns <inaya.rahmanisa@gmail.com>
2025-07-21 13:09:05 +0700

    f

---
+++ commands/get_act/xwin/qwen.sh
@@ -0,0 +1,18 @@
+python3 get_activations.py  \
+    --hf_logintoken "***REMOVED***" \
+    --model "Qwen/Qwen2.5-7B-Instruct"\
+    --dataset_name "Muennighoff/xwinograd" \
+    --split "test" \
+    --max_tokens_overzeros 100000 \
+    --max_sentence_avgs 83\
+    --batch_size 16 \
+    --kaggle_dataname_to_save "activation-xwin-qwen7-neurons" \
+    --parent_dir_to_save "workspace/" \
+    > workspace/log_qwen7.txt 2>&1
+    # --max_instances 200 \
+    # --is_update \
+    # --debug \
+    # --take_whole \
+    # --max_lang \
+    # --selected_langs "deu_Latn" "eng_Latn" "fra_Latn" "ind_Latn" "jpn_Jpan" "kor_Hang" "zsm_Latn" "nld_Latn" "por_Latn" "rus_Cyrl" "vie_Latn" "zho_Hans"\
+    # --is_predict \
---
+++ commands/int_xwinograd/xx/lape/xwin/qwen.sh
@@ -0,0 +1,19 @@
+python intervention_tasks.py \
+    --dataset_kaggle "inayarahmanisa/lsnxx-qwen7-xwinograd" \
+    --lsn_filename "qwen7_xwinograd" \
+    --ld_filename "lang_dict" \
+    --dataset_kaggle_replacer "inayarahmanisa/activation-xwin-qwen7-neurons" \
+    --replacer_filename "max.pt" \
+    --hf_token "***REMOVED***" \
+    --model_name "Qwen/Qwen2.5-7B-Instruct" \
+    --dataset_name "Muennighoff/xwinograd" \
+    --split test \
+    --replace_method fixed \
+    --operation_non_target ".1" \
+    --operation_target "=22" \
+    --metrics "acc" \
+    --kaggle_dataname_to_save "accxx-xwinograd-xwinograd" \
+    --parent_dir_to_save "" \
+    --is_update \
+    --batch_size 8 \
+    > qwen_xwinogradxx.txt 2>&1
