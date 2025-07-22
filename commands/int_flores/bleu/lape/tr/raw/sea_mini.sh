7173f83929457686c603a1347d27ad488dbebea3
inayarhmns <inaya.rahmanisa@gmail.com>
2025-07-22 14:18:08 +0700

    f

--- commands/int_flores/bleu/lape/tr/raw/sea_mini.sh
+++ commands/int_flores/bleu/lape/tr/raw/sea_mini.sh
@@ -13,7 +13,7 @@ python intervention_tasks_translate.py \
     --operation_target "=10" \
     --metrics "bleu" \
     --noncross \
-    --kaggle_dataname_to_save "bleu-flores2" \
+    --kaggle_dataname_to_save "bleu-flores2-raw" \
     --parent_dir_to_save "" \
     --selected_langs "eng_Latn-nld_Latn" "eng_Latn-ind_Latn" "eng_Latn-zsm_Latn" "eng_Latn-vie_Latn" "eng_Latn-jpn_Jpan" "eng_Latn-zho_Hans" "eng_Latn-fra_Latn" "eng_Latn-por_Latn" "eng_Latn-rus_Cyrl" "eng_Latn-est_Latn" "eng_Latn-hat_Latn" "eng_Latn-ita_Latn" "eng_Latn-quy_Latn" "eng_Latn-swh_Latn" "eng_Latn-tam_Taml" "eng_Latn-tha_Thai" "eng_Latn-tur_Latn" \
     --target_langs 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 \
